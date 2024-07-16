375a2ca1174b6a37749099280b8a2afdd29e6105
tarak ram <ramtarakchowdary3804@gmail.com>
2024-07-16 14:16:43 +0530

    removed the treenode class as it isn't useful

--- MinimumDiameter.java
+++ MinimumDiameter.java
@@ -1,14 +1,4 @@
-class TreeNode {
-    int value;
-    TreeNode left;
-    TreeNode right;
 
-    public TreeNode(int value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
-}
 
 public class MinimumDiameter {
     public static void main(String[] args) {
